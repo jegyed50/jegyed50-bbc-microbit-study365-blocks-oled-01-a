bc21ac7b78d714fc197d274e0ec65ddc8a61a90a
Egyed József <jegyed50@gmail.com>
2023-05-16 19:06:10 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,79 @@
-//OLED12864_I2C Extension
-//  G   V   SCL   SDA
-// GND 5V   P19   P20
-// Graph 128x64 point
-// Char 13x4 ASCII characters 
-
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    OLED12864_I2C.showString(0, 0, "123456789012345678901234567890123456789012345678901234567890", 1)
+/**
+ * OLED12864_I2C Extension
+ * 
+ * G   V   SCL   SDA
+ * 
+ * GND 5V   P19   P20
+ * 
+ * Graph 
+ * 
+ * Zoom = false:128x64 point
+ * 
+ * left-top corner:0,0
+ * 
+ * left-bottom corner:0,63
+ * 
+ * rigth-top corner:127,0
+ * 
+ * rigth-bottom corner:127,63
+ * 
+ * Char 13x4 
+ * 
+ * Zoom = true
+ * 
+ * left-top corner:0,0
+ * 
+ * left-bottom corner:0,31
+ * 
+ * rigth-top corner:63,0
+ * 
+ * rigth-bottom corner:63,31
+ * 
+ * Char 13x4 
+ * 
+ * Only ASCII characters chr(0) to chr(127)
+ */
+input.onButtonPressed(Button.A, function () {
+    OLED12864_I2C.showString(
+    0,
+    0,
+    "123456789012345678901234567890123456789012345678901234567890",
+    1
+    )
     basic.pause(2000)
-    OLED12864_I2C.showString(0, 0, "123456789012345678901234567890123456789012345678901234567890", 0)
+    OLED12864_I2C.showString(
+    0,
+    0,
+    "123456789012345678901234567890123456789012345678901234567890",
+    0
+    )
     basic.pause(2000)
-    OLED12864_I2C.showString(0, 0, "áéőúűóüöíÉÁŐÚŰÓÜÖÍ", 1)
+    OLED12864_I2C.showString(
+    0,
+    0,
+    "áéőúűóüöíÉÁŐÚŰÓÜÖÍ",
+    1
+    )
 })
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+input.onButtonPressed(Button.AB, function () {
     OLED12864_I2C.invert(true)
     basic.pause(2000)
     OLED12864_I2C.clear()
-    for (let index = 0; index < 8; index++) {
+    for (let index = 0; index <= 7; index++) {
         basic.pause(200)
-        OLED12864_I2C.rect(31, 0, 64 - index * 2, 31 - index * 2, 1)
+        OLED12864_I2C.rect(
+        index * 2,
+        index * 2,
+        64 - index * 2,
+        31 - index * 2,
+        1
+        )
         basic.pause(2000)
-        OLED12864_I2C.rect(index * 2, index * 2, 32 - index * 2, 32 - index * 2, 1)
-        OLED12864_I2C.rect(index * 2 + 32, index * 2 + 0, 32 + index * 2, 32 - index * 2, 1)
-        basic.pause(200)
     }
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
     OLED12864_I2C.invert(true)
-    for (let index2 = 0; index2 < 10; index2++) {
+    for (let index = 0; index < 10; index++) {
         basic.pause(500)
         OLED12864_I2C.off()
         basic.pause(500)
@@ -34,3 +81,33 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
     }
 })
 OLED12864_I2C.init(60)
+OLED12864_I2C.zoom(true)
+OLED12864_I2C.pixel(0, 0, 1)
+basic.pause(1000)
+OLED12864_I2C.pixel(0, 63, 1)
+basic.pause(1000)
+OLED12864_I2C.pixel(127, 0, 1)
+basic.pause(1000)
+OLED12864_I2C.pixel(127, 62, 1)
+basic.pause(1000)
+OLED12864_I2C.pixel(62, 31, 1)
+basic.pause(1000)
+OLED12864_I2C.clear()
+OLED12864_I2C.zoom(true)
+OLED12864_I2C.pixel(0, 0, 1)
+basic.pause(1000)
+OLED12864_I2C.pixel(0, 31, 1)
+basic.pause(1000)
+OLED12864_I2C.pixel(63, 0, 1)
+basic.pause(1000)
+OLED12864_I2C.pixel(63, 31, 1)
+basic.pause(1000)
+OLED12864_I2C.pixel(31, 16, 1)
+basic.pause(1000)
+OLED12864_I2C.showString(
+0,
+0,
+"123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890",
+1
+)
+OLED12864_I2C.zoom(false)
